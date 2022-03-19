4b9033880c50debdedf9546fd879c3b32a7daecf
MagyAshraf17 <81820152+MagyAshraf17@users.noreply.github.com>
2022-03-19 14:35:02 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+$(function() {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+$(document).ready(function() {
+    $(".butt1").mouseover(function() {
+        $(".butt1").css("background-color", "white");
+    });
+    $(".butt1").mouseover(function() {
+        $(".butt1").css("color", "blue");
+    });
+    $(".butt1").mouseout(function() {
+        $(".butt1").css("background-color", "#0c2e8a");
+    });
+    $(".butt1").mouseout(function() {
+        $(".butt1").css("color", "white");
+
+
+    });
+});
+$(document).ready(function() {
+    $(".butt2").mouseover(function() {
+        $(".butt2").css("background-color", "white");
+    });
+    $(".butt2").mouseover(function() {
+        $(".butt2").css("color", "blue");
+    });
+    $(".butt2").mouseout(function() {
+        $(".butt2").css("background-color", "#0c2e8a");
+    });
+    $(".butt2").mouseout(function() {
+        $(".butt2").css("color", "white");
+
+
+    });
+});
